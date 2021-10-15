88046c57734f721e2d1891d83c2f8989a4464b73
Darren Buttigieg <darren@onna.com>
2021-10-15 12:58:15 +0200

    Tests for info file

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -3,13 +3,13 @@ from pytest_mock import MockFixture  # type: ignore
 from component_generator.generate import generate
 
 
-def test_generate_should_call_insert_info_file_if_filepath_startswith_plus(mocker: MockFixture):
-    insert_info_spy = mocker.patch("component_generator.generate.insert_info_file")
-    generate({"+insert_info": "dummy", "generate": "dummy"}, {})
-    insert_info_spy.assert_called_once_with("insert_info", "dummy", {})
+def test_generate_should_call_append_to_info_file_if_filepath_startswith_plus(mocker: MockFixture):
+    insert_info_spy = mocker.patch("component_generator.generate.append_to_info_file")
+    generate({"+append_info": "dummy"}, {})
+    insert_info_spy.assert_called_once_with("apppend_info", "dummy", {})
 
 
 def test_generate_should_call_generate_file_if_filepath_not_startswith_plus(mocker: MockFixture):
     generate_file_spy = mocker.patch("component_generator.generate.generate_file")
-    generate({"+insert_info": "dummy", "generate": "dummy"}, {})
+    generate({"generate": "dummy"}, {})
     generate_file_spy.assert_called_once_with("generate", "dummy", {})
