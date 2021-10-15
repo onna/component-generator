27f52065f7817f8b3a127e51c44d15739a7c5767
Darren Buttigieg <darren@onna.com>
2021-10-15 10:43:47 +0200

    Insert info function (no tests)

--- src/component_generator/generate.py
+++ src/component_generator/generate.py
@@ -1,6 +1,13 @@
 import os
 from typing import Dict
 
+from component_generator.constants import START_BLOCK, END_BLOCK
+
+
+class FileNotUpdatableException(Exception):
+    def __init__(self, filepath: str):
+        super().__init__(f"File {filepath} not updatable")
+
 
 def generate(structure: Dict[str, str], settings: Dict[str, str]):
     for filepath, filedata in structure.items():
@@ -11,7 +18,27 @@ def generate(structure: Dict[str, str], settings: Dict[str, str]):
 
 
 def insert_info_file(filepath: str, filedata_part: str, settings: Dict[str, str]):
-    pass
+    populated_filepath = populate_setting_values(filepath, settings)
+    with open(populated_filepath, "r") as reader:
+        file_data = reader.read()
+    _check_info_file(file_data, populated_filepath)
+    with open(populated_filepath, "w") as writer:
+        writer.write(_append_filedata(file_data, filedata_part))
+
+
+def _check_info_file(file_data: str, filepath: str):
+    if not (START_BLOCK in file_data and END_BLOCK in file_data):
+        raise FileNotUpdatableException(filepath)
+
+
+def _append_filedata(existing_filedata: str, new_filedata: str) -> str:
+    return "\n".join(
+        (
+            existing_filedata[: existing_filedata.index(END_BLOCK)],
+            new_filedata,
+            existing_filedata[existing_filedata.index(END_BLOCK) :],
+        )
+    )
 
 
 def generate_file(filepath: str, filedata: str, settings: Dict[str, str]):
